a93c7d214b472176f3cc4a236aa4b0f256789c25
isaac Yeobah <yeboah.determined.isaac@gmail.com>
2023-10-26 09:04:44 +0000

    update

---
+++ config/migrations/20231026081632_posts.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+      
+    return knex.schema.createTable('posts', function (table) {
+        table.increments('id').primary();
+        table.string('title', 255).notNullable();
+        table.string('article', 255);
+        table.integer('user_id', 255).notNullable();
+        table.integer('comments', 255);
+        table.timestamp(true, true);
+    });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('posts');
+  
+};
---
+++ config/migrations/20231026081640_comments.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+
+    return knex.schema.createTable('comments', function (table) {
+      
+    table.increments('id').primary();
+
+    table.integer('user_id', 255).notNullable();
+    table.integer('post_id', 255).notNullable();
+    table.integer('comment', 255);
+    table.timestamp(true, true);
+});
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('comments');
+  
+};
